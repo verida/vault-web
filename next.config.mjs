ae4eadf117d1831380181466736d1406053b0118
Andy Nguyen <andy@verida.io>
2024-05-01 13:11:03 +0700

    fix: fix font loader

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,19 @@ const nextConfig = {
     config.node = {
       __dirname: true,
     };
+    
+    config.module.rules.push({
+      test: /\.(woff|woff2|eot|ttf|otf)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          publicPath: '/_next/static/fonts/',
+          outputPath: `${isServer ? '../' : ''}static/fonts/`,
+        },
+      },
+    });
+
     return config;
   },
   images: {
