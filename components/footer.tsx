9faa275b31a900282fc861f3e0e64f101f697051
InfiniteAengus <infiniteaengus@gmail.com>
2024-06-12 16:34:26 -0400

    updated header for mobile

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,17 +1,16 @@
 import { Logo } from "@/components/logo";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export const Footer = () => {
   return (
-    <div className="fixed bottom-0 w-full p-4 border-t bg-slate-100 lg:mx[108px]">
-      <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
-        <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
-          <Button size="sm" variant="ghost">
-            Privacy Policy
-          </Button>
-          <Button size="sm" variant="ghost">
-            Terms of Service
-          </Button>
+    <div className='fixed bottom-0 w-full p-4 lg:mx[108px]'>
+      <div className='md:max-w-screen-2xl mx-auto flex items-center w-full justify-between'>
+        <div className='flex items-center justify-between w-full'>
+          <p className='text-xs text-gray-500'>© 2024 Verida Vault</p>
+          <Link href='/terms' className='text-xs text-gray-500'>
+            Terms & Conditions
+          </Link>
         </div>
       </div>
     </div>
--- components/logo.tsx
+++ components/logo.tsx
@@ -3,14 +3,9 @@ import Image from "next/image";
 
 export const Logo = () => {
   return (
-    <Link href="/">
-      <div className="hover:opacity-75 transition items-center gap-x-2 hidden md:flex">
-        <Image
-          src="/logo.svg"
-          alt="Logo"
-          height={32}
-          width={95}
-        />
+    <Link href='/'>
+      <div className='hover:opacity-75 transition items-center gap-x-2 md:flex'>
+        <Image src='/logo.svg' alt='Logo' height={32} width={95} />
       </div>
     </Link>
   );
