b5af708013db50b65ebcbc54c790e0849e80299b
InfiniteAengus <infiniteaengus@gmail.com>
2024-06-21 01:11:10 -0400

    removed patches

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,19 @@ const nextConfig = {
     config,
     { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
   ) => {
+    config.module.rules.push({
+      test: /\.(ttf|otf|eot|woff|woff2)$/,
+      use: [
+        {
+          loader: "file-loader",
+          options: {
+            name: "[name].[ext]",
+            outputPath: "static/fonts/",
+            publicPath: "/_next/static/fonts/",
+          },
+        },
+      ],
+    });
     config.node = {
       __dirname: true,
     };
