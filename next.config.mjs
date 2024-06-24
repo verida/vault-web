e5bb3e45a3196a8c1858bf3b4ecd37ad5f1b636a
Andy Nguyen <andy@verida.io>
2024-06-24 10:11:59 +0700

    fix: fix the account-web-vault patch

--- next.config.mjs
+++ next.config.mjs
@@ -24,6 +24,19 @@ const nextConfig = {
     config.node = {
       __dirname: true,
     };
+
+    config.module.rules.push({
+      test: /\.(woff|woff2|eot|ttf|otf)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          publicPath: '/_next/static/fonts/',
+          outputPath: `${isServer ? '../' : ''}static/fonts/`,
+        },
+      },
+    });
+    
     return config;
   },
 };
