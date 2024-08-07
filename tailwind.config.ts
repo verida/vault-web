2a5fe67a357615f406d362160b15df417348b665
InfiniteAengus <infiniteaengus@gmail.com>
2024-08-07 10:56:45 +0900

    udpate tailwind config file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,10 +2,7 @@ import type { Config } from "tailwindcss";
 
 const config = {
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./src/**/*.{js,ts,jsx,tsx, mdx}",
 	],
   prefix: "",
   theme: {
