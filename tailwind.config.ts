ed3f41ce023a09f1dc85ba597d3860cd5068e93e
aurelticot <aurelticot.dev@gmail.com>
2024-08-08 14:38:50 +1000

    chore: update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,8 +2,10 @@ import type { Config } from "tailwindcss";
 
 const config = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx, mdx}",
-	],
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   prefix: "",
   theme: {
     container: {
