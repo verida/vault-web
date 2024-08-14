f76877977d390af610d5a98fa8c1816d787c8de9
InfiniteAengus <infiniteaengus@gmail.com>
2024-08-14 10:25:59 +0900

    add meta tags

--- features/verida/constants/index.ts
+++ features/verida/constants/index.ts
@@ -9,3 +9,10 @@ export const VERIDA_DID_REGEXP =
 export const VERIDA_CONNECT_SESSION_LOCAL_STORAGE_KEY = "_verida_auth_context"
 export const CLEAR_SESSION_AFTER_MAINNET_UPGRADE_LOCAL_STORAGE_KEY =
   "_session_cleared_after_mainnet_upgrade"
+
+export const VERIDA_VAULT_TITLE = "Verida Vault"
+export const VERIDA_VAULT_DESCRIPTION =
+  "Manage your crypto, encrypted personal data and zero knowledge credentials with the Verida Vault App."
+export const VERIDA_VAULT_HERO_IMAGE =
+  "https://develop.d25282gg40x7nq.amplifyapp.com/_next/image?url=%2Fimages%2Fdata-page.png&w=1920&q=75"
+export const VERIDA_VAULT_URL = "https://develop.d25282gg40x7nq.amplifyapp.com"
