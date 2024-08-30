e4885a3591711e83df02b071aa9f706d11260643
aurelticot <aurelticot.dev@gmail.com>
2024-08-30 10:05:03 +1000

    feat: update style of processing state of assistant answer

--- app/(root)/assistant/_components/assistant-chat-message.tsx
+++ app/(root)/assistant/_components/assistant-chat-message.tsx
@@ -41,20 +41,28 @@ function AssistantChatAssistantMessage(
   return (
     <div className="text-start">
       <div className="flex flex-row gap-3 rounded-xl border bg-white p-4 sm:gap-4">
-        <Avatar
-          className={cn(
-            "size-6 shrink-0 bg-ai-assistant-gradient p-1 text-white sm:size-8",
-            isProcessing ? "animate-pulse" : ""
-          )}
-        >
-          <VeridaNetworkLogo className="size-4 sm:size-6" />
+        <Avatar className="relative size-6 shrink-0 p-1 text-white sm:size-8">
+          <div
+            className={cn(
+              "absolute inset-0 bg-ai-assistant-gradient",
+              isProcessing ? "animate-spin-slow" : ""
+            )}
+          />
+          <div className="absolute inset-0 flex flex-row items-center justify-center">
+            <VeridaNetworkLogo className="size-4 sm:size-6" />
+          </div>
         </Avatar>
         <div className="prose prose-sm max-w-full flex-1 overflow-x-auto pt-1">
           {isProcessing ? (
-            <div className="flex w-full flex-col gap-2">
-              <Skeleton className="h-3.5 w-full rounded-full" />
-              <Skeleton className="h-3.5 w-11/12 rounded-full" />
-              <Skeleton className="h-3.5 w-1/5 rounded-full" />
+            <div className="flex w-full flex-col">
+              <div className="flex flex-row gap-2">
+                <Skeleton className="my-[0.3125rem] h-3.5 w-1/12 rounded-full" />
+                <Skeleton className="my-[0.3125rem] h-3.5 w-9/12 rounded-full" />
+                <Skeleton className="my-[0.3125rem] h-3.5 flex-1 rounded-full" />
+              </div>
+              <div className="flex flex-row gap-2">
+                <Skeleton className="my-[0.3125rem] h-3.5 w-7/12 rounded-full" />
+              </div>
             </div>
           ) : (
             <ReactMarkdown
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -226,6 +226,7 @@ const config = {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "spin-slow": "spin 4s linear infinite",
       },
     },
   },
