4fcb58019df70783185fbc52c6de53a68257cb02
aurelticot <aurelticot.dev@gmail.com>
2024-09-01 00:13:10 +1000

    fix: fix overall layout scroll issue

--- src/app/(connected)/layout.tsx
+++ src/app/(connected)/layout.tsx
@@ -14,8 +14,8 @@ export default function AppLayout(props: AppLayoutProps) {
       <AppProviders>
         <div className="flex h-dvh flex-col">
           <AppHeader />
-          <div className="flex flex-1 flex-col items-center justify-center">
-            <main className="w-full max-w-screen-2xl flex-1 overflow-y-auto px-4 pb-0 pt-6 md:px-6 md:pt-10 xl:px-8">
+          <div className="flex flex-1 flex-col items-center justify-center overflow-y-auto">
+            <main className="h-full w-full max-w-screen-2xl px-4 pb-0 pt-6 md:px-6 md:pt-10 xl:px-8">
               {children}
             </main>
           </div>
