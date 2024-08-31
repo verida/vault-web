b8b4857529897f3d14ccc7bd1748484c00ebce8a
aurelticot <aurelticot.dev@gmail.com>
2024-08-31 21:00:06 +1000

    feat: add global style

--- src/app/(connected)/layout.tsx
+++ src/app/(connected)/layout.tsx
@@ -12,7 +12,7 @@ export default function AppLayout(props: AppLayoutProps) {
   return (
     <AppConnectionHandler>
       <AppProviders>
-        <div className="flex h-dvh flex-col bg-background">
+        <div className="flex h-dvh flex-col">
           <AppHeader />
           <div className="flex flex-1 flex-col items-center justify-center">
             <main className="w-full max-w-screen-2xl flex-1 overflow-y-auto px-4 pb-0 pt-6 md:px-6 md:pt-10 xl:px-8">
