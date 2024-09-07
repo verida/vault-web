f75f02fd47d43dbf92fe09ff59b7795992ad232b
aurelticot <aurelticot.dev@gmail.com>
2024-09-07 11:33:22 +1000

    fix: fix redirect after data connection disconnect

--- src/features/data-connections/hooks/use-disconnect-data-connection.ts
+++ src/features/data-connections/hooks/use-disconnect-data-connection.ts
@@ -1,8 +1,9 @@
-import { useMutation } from "@tanstack/react-query"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
 
 import { commonConfig } from "@/config/common"
 import { DataConnectionsQueryKeys } from "@/features/data-connections/queries"
 import { disconnectDataConnection } from "@/features/data-connections/utils"
+import { wait } from "@/utils/misc"
 
 type DisconnectDataConnectionVariables = {
   providerId: string
@@ -10,6 +11,8 @@ type DisconnectDataConnectionVariables = {
 }
 
 export function useDisconnectDataConnection() {
+  const queryClient = useQueryClient()
+
   const { mutate, mutateAsync, ...mutation } = useMutation({
     mutationFn: ({
       providerId,
@@ -20,11 +23,18 @@ export function useDisconnectDataConnection() {
         accountId,
         commonConfig.PRIVATE_DATA_API_PRIVATE_KEY
       ),
+    onSuccess: () => {
+      wait(1000).then(() => {
+        // Putting a timer to leave time for the other mutation onSuccess hook
+        // to update the UI
+        queryClient.invalidateQueries({
+          queryKey: DataConnectionsQueryKeys.invalidateDataConnections(),
+        })
+      })
+    },
     meta: {
       logCategory: "DataConnections",
       errorMessage: "Error disconnecting data connection",
-      onSettledInvalidationQueryKeys:
-        DataConnectionsQueryKeys.invalidateDataConnections(),
     },
   })
 
