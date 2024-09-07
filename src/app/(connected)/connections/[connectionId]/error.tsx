c69ecb430f3e93c61d67340618c95b86d403dc1e
aurelticot <aurelticot.dev@gmail.com>
2024-09-07 12:25:23 +1000

    feat: add basic loading and error page to data connections routes

---
+++ src/app/(connected)/connections/[connectionId]/error.tsx
@@ -0,0 +1,5 @@
+"use client"
+
+export default function ConnectionErrorPage() {
+  return <div>Error</div>
+}
---
+++ src/app/(connected)/connections/summary/error.tsx
@@ -0,0 +1,5 @@
+"use client"
+
+export default function ConnectionsSummaryErrorPage() {
+  return <div>Error</div>
+}
