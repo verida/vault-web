44b98015f53c40e36c7da6cb64125f05cc9b20c8
aurelticot <aurelticot.dev@gmail.com>
2024-09-09 18:16:10 +1000

    feat: add new databases in data tab

--- src/features/data/constants/index.ts
+++ src/features/data/constants/index.ts
@@ -16,7 +16,7 @@ export const DATABASE_DEFS: DatabaseDefinition[] = [
     databaseVaultName: "social_contact",
   },
   {
-    id: "social-following",
+    id: "social-followings",
     title: "Social Following",
     titlePlural: "Social Followings",
     color: "#7A78E5",
@@ -28,5 +28,33 @@ export const DATABASE_DEFS: DatabaseDefinition[] = [
     titlePlural: "Social Posts",
     color: "#EE7D91",
     databaseVaultName: "social_post",
+  },
+  {
+    id: "emails",
+    title: "Email",
+    titlePlural: "Emails",
+    color: "#FFB347",
+    databaseVaultName: "social_email",
+  },
+  {
+    id: "favourites",
+    title: "Favourite",
+    titlePlural: "Favourites",
+    color: "#FF69B4",
+    databaseVaultName: "favourite",
+  },
+  {
+    id: "chat-groups",
+    title: "Chat group",
+    titlePlural: "Chat groups",
+    color: "#20B2AA",
+    databaseVaultName: "social_chat_group",
+  },
+  {
+    id: "chat-messages",
+    title: "Chat message",
+    titlePlural: "Chat messages",
+    color: "#9370DB",
+    databaseVaultName: "social_chat_message",
   },
 ]
