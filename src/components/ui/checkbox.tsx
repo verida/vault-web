3a1f3c2169a6d224e7326b171ec286f8106620f8
aurelticot <aurelticot.dev@gmail.com>
2024-09-10 14:16:35 +1000

    feat: update style of focused UI elements

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -13,7 +13,7 @@ const Checkbox = React.forwardRef<
   <CheckboxPrimitive.Root
     ref={ref}
     className={cn(
-      "peer h-5 w-5 shrink-0 rounded-sm border bg-muted ring-offset-surface focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-accent",
+      "peer h-5 w-5 shrink-0 rounded-sm border bg-muted ring-offset-surface focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-accent",
       className
     )}
     {...props}
