0ebe71256d39aca6e56faf9b32b1745b37ecb79b
aurelticot <aurelticot.dev@gmail.com>
2024-09-23 20:18:07 +1000

    feat: generate app version file

--- src/config/common.ts
+++ src/config/common.ts
@@ -1,4 +1,5 @@
 import { CommonConfigSchema } from "@/config/schemas"
+import { version } from "@/config/version"
 
 const commonConfigCheckResult = CommonConfigSchema.safeParse({
   // Have to pass the variables one-by-one on the client because they are set
@@ -23,6 +24,7 @@ const commonConfigCheckResult = CommonConfigSchema.safeParse({
   FEATURE_FLAG_DATA_CONNECTIONS_LOGS_ENABLED:
     process.env.NEXT_PUBLIC_FEATURE_FLAG_DATA_CONNECTIONS_LOGS_ENABLED,
   isClient: !(typeof window === "undefined"),
+  appVersion: version,
 })
 
 if (!commonConfigCheckResult.success) {
