b4c016dd7c5d329f923a41f8823af1c0ca8cde44
aurelticot <aurelticot.dev@gmail.com>
2024-10-01 12:22:42 +1000

    fix: handle search params in connection handler redirect

--- src/app/(connected)/_components/app-connection-handler.tsx
+++ src/app/(connected)/_components/app-connection-handler.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { redirect, usePathname } from "next/navigation"
+import { redirect, usePathname, useSearchParams } from "next/navigation"
 
 import { useVerida } from "@/features/verida"
 
@@ -12,13 +12,16 @@ export function AppConnectionHandler(props: AppConnectionHandlerProps) {
   const { children } = props
 
   const pathName = usePathname()
+  const searchParams = useSearchParams()
   const { isConnected } = useVerida()
 
   if (!isConnected) {
-    const encodedRedirectPath = encodeURIComponent(pathName)
+    const fullPath = `${pathName}${searchParams.toString() ? `?${searchParams.toString()}` : ""}`
+    const encodedRedirectPath = encodeURIComponent(fullPath)
     // Ensure to use the same `redirectPath` query parameter as in `RootConnectionHandler`.
     redirect(`/?redirectPath=${encodedRedirectPath}`)
   }
 
   return <>{children}</>
 }
+AppConnectionHandler.displayName = "AppConnectionHandler"
