188bf722339068b19c4de1d77f22cddb522bd189
aurelticot <aurelticot.dev@gmail.com>
2024-10-19 10:27:54 +1100

    feat: implement search data feature

---
+++ src/features/data-search/constants.ts
@@ -0,0 +1,9 @@
+import { SearchType } from "@/features/data-search/types"
+
+export const DEFAULT_SELECTED_SEARCH_TYPES: SearchType[] = [
+  "emails",
+  "favorites",
+  "followed_pages",
+  "messages",
+  "posts",
+]
