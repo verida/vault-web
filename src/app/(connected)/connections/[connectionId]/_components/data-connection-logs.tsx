3d874e0427e7fb332ebb3ca0b5e905b228be09b4
aurelticot <aurelticot.dev@gmail.com>
2024-10-31 09:41:43 +1100

    feat: create generic data table component

--- src/app/(connected)/connections/[connectionId]/_components/data-connection-logs.tsx
+++ src/app/(connected)/connections/[connectionId]/_components/data-connection-logs.tsx
@@ -6,26 +6,7 @@ import { useState } from "react"
 
 import { dataConnectionLogsTableColumns } from "@/components/data-connections/logs-table/data-connection-logs-table-columns"
 import { DataConnectionLogsTableRow } from "@/components/data-connections/logs-table/data-connection-logs-table-row"
-import { DataTableHeader } from "@/components/data-table/data-table-header"
-import { DataTablePagination } from "@/components/data-table/data-table-pagination"
-import {
-  EmptyState,
-  EmptyStateDescription,
-  EmptyStateImage,
-  EmptyStateTitle,
-} from "@/components/ui/empty-state"
-import {
-  ErrorBlock,
-  ErrorBlockDescription,
-  ErrorBlockImage,
-  ErrorBlockTitle,
-} from "@/components/ui/error"
-import {
-  LoadingBlock,
-  LoadingBlockDescription,
-  LoadingBlockSpinner,
-  LoadingBlockTitle,
-} from "@/components/ui/loading"
+import { DataTable } from "@/components/data-table/data-table"
 import { EMPTY_VALUE_FALLBACK } from "@/constants/misc"
 import { useDataConnectionLogs } from "@/features/data-connections/hooks/use-data-connection-logs"
 import {
@@ -36,7 +17,6 @@ import { DataTableColumnAlignFeature } from "@/features/data-table/data-table-co
 import { DataTableColumnClassNameFeature } from "@/features/data-table/data-table-column-classname-feature"
 import { DataTablePaginationSizeValue } from "@/features/data-table/types"
 import { VeridaRecord } from "@/features/verida-database/types"
-import { cn } from "@/styles/utils"
 
 const PAGINATION_SIZE_DEFAULT = 5
 
@@ -61,10 +41,10 @@ const getRowId = (record: VeridaRecord<DataConnectionSyncLog>) => record._id
 
 export type DataConnectionLogsProps = {
   connection: DataConnection
-} & React.ComponentProps<"div">
+} & Pick<React.ComponentProps<typeof DataTable>, "className">
 
 export function DataConnectionLogs(props: DataConnectionLogsProps) {
-  const { connection, className, ...divProps } = props
+  const { connection, className } = props
 
   const [pagination, setPagination] = useState<PaginationState>({
     pageIndex: 0,
@@ -108,59 +88,22 @@ export function DataConnectionLogs(props: DataConnectionLogsProps) {
   })
 
   return (
-    <div className={cn("flex flex-col gap-0", className)} {...divProps}>
-      <div className="hidden md:block">
-        {table.getHeaderGroups().map((headerGroup) => (
-          <DataTableHeader
-            key={headerGroup.id}
-            columnHeaders={headerGroup.headers}
-          />
-        ))}
-      </div>
-      {table.getRowModel().rows?.length ? (
-        <ul className="flex flex-col gap-3">
-          {table.getRowModel().rows.map((row) => (
-            <li key={row.id}>
-              <DataConnectionLogsTableRow row={row} hideConnectionColumn />
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <div className="flex flex-1 flex-col items-center justify-center">
-          {isLoading ? (
-            <LoadingBlock>
-              <LoadingBlockSpinner />
-              <LoadingBlockTitle>Loading connections logs...</LoadingBlockTitle>
-              <LoadingBlockDescription>
-                Please wait while we load the connections logs.
-              </LoadingBlockDescription>
-            </LoadingBlock>
-          ) : isError ? (
-            <ErrorBlock>
-              <ErrorBlockImage />
-              <ErrorBlockTitle>Error</ErrorBlockTitle>
-              <ErrorBlockDescription>
-                There was an error getting the connections logs. Please try
-                again later.
-              </ErrorBlockDescription>
-            </ErrorBlock>
-          ) : (
-            <EmptyState>
-              <EmptyStateImage />
-              <EmptyStateTitle>No connections logs yet</EmptyStateTitle>
-              <EmptyStateDescription>
-                {`There hasn't been any sync activities on your data connections yet.`}
-              </EmptyStateDescription>
-            </EmptyState>
-          )}
-        </div>
+    <DataTable
+      table={table}
+      rowComponent={(row) => (
+        <DataConnectionLogsTableRow row={row} hideConnectionColumn />
       )}
-      <DataTablePagination
-        table={table}
-        paginationSizes={PAGINATION_SIZES}
-        className="mt-6"
-      />
-    </div>
+      className={className}
+      paginationSizes={PAGINATION_SIZES}
+      isLoading={isLoading}
+      isError={isError}
+      loadingTitle="Loading connections logs..."
+      loadingDescription="Please wait while we load the connections logs."
+      errorTitle="Error"
+      errorDescription="There was an error getting the connections logs. Please try again later."
+      emptyStateTitle="No connections logs yet"
+      emptyStateDescription="There hasn't been any sync activities on your data connections yet."
+    />
   )
 }
 DataConnectionLogs.displayName = "DataConnectionLogs"
--- src/app/(connected)/connections/summary/logs/page.tsx
+++ src/app/(connected)/connections/summary/logs/page.tsx
@@ -9,27 +9,8 @@ import { useState } from "react"
 
 import { dataConnectionLogsTableColumns } from "@/components/data-connections/logs-table/data-connection-logs-table-columns"
 import { DataConnectionLogsTableRow } from "@/components/data-connections/logs-table/data-connection-logs-table-row"
-import { DataTableHeader } from "@/components/data-table/data-table-header"
-import { DataTablePagination } from "@/components/data-table/data-table-pagination"
+import { DataTable } from "@/components/data-table/data-table"
 import { PageWrapper } from "@/components/page-wrapper"
-import {
-  EmptyState,
-  EmptyStateDescription,
-  EmptyStateImage,
-  EmptyStateTitle,
-} from "@/components/ui/empty-state"
-import {
-  ErrorBlock,
-  ErrorBlockDescription,
-  ErrorBlockImage,
-  ErrorBlockTitle,
-} from "@/components/ui/error"
-import {
-  LoadingBlock,
-  LoadingBlockDescription,
-  LoadingBlockSpinner,
-  LoadingBlockTitle,
-} from "@/components/ui/loading"
 import { EMPTY_VALUE_FALLBACK } from "@/constants/misc"
 import { useDataConnectionsLogs } from "@/features/data-connections/hooks/use-data-connections-logs"
 import { DataConnectionSyncLog } from "@/features/data-connections/types"
@@ -83,57 +64,19 @@ export default function ConnectionsLogsPage() {
 
   return (
     <PageWrapper pageTitle="Connection Logs">
-      <div className="flex flex-1 flex-col gap-0">
-        <div className="hidden md:block">
-          {table.getHeaderGroups().map((headerGroup) => (
-            <DataTableHeader
-              key={headerGroup.id}
-              columnHeaders={headerGroup.headers}
-            />
-          ))}
-        </div>
-        {table.getRowModel().rows?.length ? (
-          <ul className="flex flex-col gap-3">
-            {table.getRowModel().rows.map((row) => (
-              <li key={row.id}>
-                <DataConnectionLogsTableRow row={row} />
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <div className="flex flex-1 flex-col items-center justify-center">
-            {isLoading ? (
-              <LoadingBlock>
-                <LoadingBlockSpinner />
-                <LoadingBlockTitle>
-                  Loading connections logs...
-                </LoadingBlockTitle>
-                <LoadingBlockDescription>
-                  Please wait while we load the connections logs.
-                </LoadingBlockDescription>
-              </LoadingBlock>
-            ) : isError ? (
-              <ErrorBlock>
-                <ErrorBlockImage />
-                <ErrorBlockTitle>Error</ErrorBlockTitle>
-                <ErrorBlockDescription>
-                  There was an error getting the connections logs. Please try
-                  again later.
-                </ErrorBlockDescription>
-              </ErrorBlock>
-            ) : (
-              <EmptyState>
-                <EmptyStateImage />
-                <EmptyStateTitle>No connections logs yet</EmptyStateTitle>
-                <EmptyStateDescription>
-                  {`There hasn't been any sync activities on your data connections yet.`}
-                </EmptyStateDescription>
-              </EmptyState>
-            )}
-          </div>
-        )}
-        <DataTablePagination table={table} className="mt-6" />
-      </div>
+      <DataTable
+        table={table}
+        rowComponent={(row) => <DataConnectionLogsTableRow row={row} />}
+        className="flex-1"
+        isLoading={isLoading}
+        isError={isError}
+        loadingTitle="Loading connections logs..."
+        loadingDescription="Please wait while we load the connections logs."
+        errorTitle="Error"
+        errorDescription="There was an error getting the connections logs. Please try again later."
+        emptyStateTitle="No connections logs yet"
+        emptyStateDescription="There hasn't been any sync activities on your data connections yet."
+      />
     </PageWrapper>
   )
 }
