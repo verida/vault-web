cab2a9fb9969b416cddb89beb2ef710e8d3a81b6
aurelticot <aurelticot.dev@gmail.com>
2024-11-14 16:30:03 +1100

    feat: list saved prompts in prompts

--- src/app/(connected)/assistant/_components/assistant-user-input-prompts-combobox.tsx
+++ src/app/(connected)/assistant/_components/assistant-user-input-prompts-combobox.tsx
@@ -1,9 +1,8 @@
 "use client"
 
-import { MessageSquareMoreIcon } from "lucide-react"
+import { ArrowUpRightIcon, MessageSquareMoreIcon } from "lucide-react"
 import { useCallback, useState } from "react"
 
-import { EditIcon } from "@/components/icons/edit-icon"
 import { Button } from "@/components/ui/button"
 import {
   Command,
@@ -12,6 +11,7 @@ import {
   CommandInput,
   CommandItem,
   CommandList,
+  CommandLoading,
 } from "@/components/ui/command"
 import {
   Popover,
@@ -20,6 +20,7 @@ import {
 } from "@/components/ui/popover"
 import { SUGGESTED_INPUTS } from "@/features/assistants/constants"
 import { useAssistants } from "@/features/assistants/hooks/use-assistants"
+import { useSavedAssistantPrompts } from "@/features/assistants/hooks/use-saved-assistant-prompts"
 import { AssistantUserInput } from "@/features/assistants/types"
 import { cn } from "@/styles/utils"
 
@@ -36,6 +37,11 @@ export function AssistantUserInputPromptsCombobox(
 
   const { setAndProcessUserInput, updateUserPrompt } = useAssistants()
 
+  // TODO: Handle pagination, filter and sort. Link this to the value in the
+  // CommandInput and turn the whole Command to a controlled component
+  const { savedPrompts, isLoading: isLoadingSavedPrompts } =
+    useSavedAssistantPrompts()
+
   const handleItemClick = useCallback(
     async (input: AssistantUserInput) => {
       setAndProcessUserInput(input)
@@ -44,7 +50,7 @@ export function AssistantUserInputPromptsCombobox(
     [setAndProcessUserInput]
   )
 
-  const handleEditItemClick = useCallback(
+  const handleSetPromptClick = useCallback(
     async (input: AssistantUserInput) => {
       updateUserPrompt(input.prompt)
       onClickEdit?.(input)
@@ -79,34 +85,47 @@ export function AssistantUserInputPromptsCombobox(
           </div>
           <CommandList>
             <CommandEmpty>No results found</CommandEmpty>
-            <CommandGroup heading="Suggested by Verida">
-              {SUGGESTED_INPUTS.map((recommendation, index) => (
-                <CommandItem
-                  key={index}
-                  value={recommendation.input.prompt}
-                  onSelect={() => {
-                    handleItemClick(recommendation.input)
-                  }}
-                  className="cursor-pointer py-1 pl-2 pr-1"
-                >
-                  <div className="flex flex-row items-center gap-2">
-                    <div className="flex-1 truncate">
-                      {recommendation.label}
-                    </div>
-                    <div className="shrink-0">
-                      <Button
-                        variant="ghost"
-                        size="icon"
-                        onClick={(e) => {
-                          e.stopPropagation()
-                          handleEditItemClick(recommendation.input)
+            {isLoadingSavedPrompts ||
+            (savedPrompts && savedPrompts.length > 0) ? (
+              <CommandGroup heading="Saved">
+                {isLoadingSavedPrompts ? (
+                  <CommandLoading>Loading...</CommandLoading>
+                ) : savedPrompts ? (
+                  <>
+                    {savedPrompts.map((savedPrompt) => (
+                      <PromptItem
+                        key={savedPrompt._id}
+                        label={savedPrompt.name ?? savedPrompt.data.prompt}
+                        prompt={savedPrompt.data.prompt}
+                        onSelect={() => {
+                          handleItemClick({
+                            prompt: savedPrompt.data.prompt,
+                          })
                         }}
-                      >
-                        <EditIcon className="size-5 sm:size-6" />
-                      </Button>
-                    </div>
-                  </div>
-                </CommandItem>
+                        onClickSetPrompt={() => {
+                          handleSetPromptClick({
+                            prompt: savedPrompt.data.prompt,
+                          })
+                        }}
+                      />
+                    ))}
+                  </>
+                ) : null}
+              </CommandGroup>
+            ) : null}
+            <CommandGroup heading="Suggested by Verida">
+              {SUGGESTED_INPUTS.map((suggestedInput, index) => (
+                <PromptItem
+                  key={index}
+                  label={suggestedInput.label}
+                  prompt={suggestedInput.input.prompt}
+                  onSelect={() => {
+                    handleItemClick(suggestedInput.input)
+                  }}
+                  onClickSetPrompt={() => {
+                    handleSetPromptClick(suggestedInput.input)
+                  }}
+                />
               ))}
             </CommandGroup>
           </CommandList>
@@ -117,3 +136,39 @@ export function AssistantUserInputPromptsCombobox(
 }
 AssistantUserInputPromptsCombobox.displayName =
   "AssistantUserInputPromptsCombobox"
+
+type PromptItemProps = {
+  label: string
+  prompt: string
+  onSelect: () => void
+  onClickSetPrompt: () => void
+}
+
+function PromptItem(props: PromptItemProps) {
+  const { label, prompt, onSelect, onClickSetPrompt } = props
+
+  return (
+    <CommandItem
+      value={prompt}
+      onSelect={onSelect}
+      className="cursor-pointer py-1 pl-2 pr-1"
+    >
+      <div className="flex flex-row items-center gap-2">
+        <div className="flex-1 truncate">{label}</div>
+        <div className="shrink-0">
+          <Button
+            variant="ghost"
+            size="icon"
+            onClick={(e) => {
+              e.stopPropagation()
+              onClickSetPrompt()
+            }}
+          >
+            <ArrowUpRightIcon className="size-5 sm:size-6" />
+          </Button>
+        </div>
+      </div>
+    </CommandItem>
+  )
+}
+PromptItem.displayName = "PromptItem"
