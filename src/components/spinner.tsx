853631748b3366f55d732b7b842586956f999209
aurelticot <aurelticot.dev@gmail.com>
2024-11-15 11:06:48 +1100

    fix: move spinner mask outside the spinner div

--- src/components/spinner.tsx
+++ src/components/spinner.tsx
@@ -22,12 +22,11 @@ export function Spinner(props: SpinnerProps) {
   }, [spinnerClassName])
 
   return (
-    <div className={className} {...divProps}>
+    <div className={cn("relative", className)} {...divProps}>
       <div
         className={cn(
           "size-20 animate-spin rounded-full bg-gradient-conic",
-          spinnerClassName,
-          "relative"
+          spinnerClassName
         )}
         style={{
           mask: "url(#spinner-mask) no-repeat center",
@@ -35,22 +34,21 @@ export function Spinner(props: SpinnerProps) {
           WebkitMaskSize: "100%",
           maskSize: "100%",
         }}
+      ></div>
+      <svg
+        ref={svgRef}
+        width="0"
+        height="0"
+        viewBox={`0 0 ${svgSize} ${svgSize}`}
+        className={cn("size-20", spinnerClassName, "absolute inset-0")}
       >
-        <svg
-          ref={svgRef}
-          width="0"
-          height="0"
-          viewBox={`0 0 ${svgSize} ${svgSize}`}
-          className={cn("size-20", spinnerClassName, "absolute inset-0")}
-        >
-          <defs>
-            <mask id="spinner-mask" x="0" y="0" width="1" height="1">
-              <rect x="0" y="0" width="100%" height="100%" fill="white" />
-              <circle cx="50%" cy="50%" r="37.5%" fill="black" />
-            </mask>
-          </defs>
-        </svg>
-      </div>
+        <defs>
+          <mask id="spinner-mask" x="0" y="0" width="1" height="1">
+            <rect x="0" y="0" width="100%" height="100%" fill="white" />
+            <circle cx="50%" cy="50%" r="37.5%" fill="black" />
+          </mask>
+        </defs>
+      </svg>
     </div>
   )
 }
