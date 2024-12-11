70b27f5d9adb9a92e777f1bd83663bb69be4deef
aurelticot <aurelticot.dev@gmail.com>
2024-12-11 15:07:33 +1100

    feat: put oauth under feature flag

--- src/app/oauth/layout.tsx
+++ src/app/oauth/layout.tsx
@@ -1,9 +1,17 @@
+import { notFound } from "next/navigation"
+
+import { featureFlags } from "@/config/features"
+
 type OAuthLayoutProps = {
   children: React.ReactNode
 }
 
 export default function OAuthLayout(props: OAuthLayoutProps) {
   const { children } = props
+
+  if (!featureFlags.veridaOauth.enabled) {
+    return notFound()
+  }
 
   return (
     <div className="flex h-full w-full flex-col items-center justify-center gap-8 p-4">
