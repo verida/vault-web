ec41db8b573040a677557ecd0c2a837bf980641b
aurelticot <aurelticot.dev@gmail.com>
2025-01-10 16:55:49 +1100

    feat: add breadcrumb UI component

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -74,7 +74,7 @@ export const CardFooter = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <footer
     ref={ref}
-    className={cn("flex items-center p-6 pt-0", className)}
+    className={cn("flex flex-row items-center p-6 pt-0", className)}
     {...props}
   />
 ))
