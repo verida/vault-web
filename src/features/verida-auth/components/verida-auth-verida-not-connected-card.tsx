ed3304e9d2722af4bc6b3b9f39fd9668547a845b
aurelticot <aurelticot.dev@gmail.com>
2025-02-12 19:24:26 +1100

    feat: update message in Verida Auth not connected card

--- src/features/verida-auth/components/verida-auth-verida-not-connected-card.tsx
+++ src/features/verida-auth/components/verida-auth-verida-not-connected-card.tsx
@@ -2,12 +2,8 @@
 
 import Link from "next/link"
 
+import { Typography } from "@/components/typography"
 import { Card, CardBody } from "@/components/ui/card"
-import {
-  ErrorBlock,
-  ErrorBlockDescription,
-  ErrorBlockTitle,
-} from "@/components/ui/error"
 import { VeridaConnectButton } from "@/components/verida/verida-connect-button"
 import { VeridaAuthCardHeader } from "@/features/verida-auth/components/verida-auth-card-header"
 import { ValidVeridaAuthRequest } from "@/features/verida-auth/types"
@@ -26,29 +22,38 @@ export function VeridaAuthVeridaNotConnectedCard(
   return (
     <Card className={cn("", className)} {...cardProps}>
       <VeridaAuthCardHeader request={request} className="shrink-0" />
-      <CardBody className="flex flex-1 flex-col gap-4 overflow-y-auto">
-        <div className="flex flex-col items-center gap-4">
-          <ErrorBlock className="my-8">
-            <ErrorBlockTitle variant="heading-4" component="p">
-              You are not connected
-            </ErrorBlockTitle>
-            <ErrorBlockDescription>
-              Learn more about how the Verida Network helps you take back
-              control of your personal data.
-            </ErrorBlockDescription>
-            <ErrorBlockDescription>
-              Check{" "}
+      <CardBody className="flex-1 overflow-y-auto pt-4 sm:pt-8">
+        <div className="flex flex-col items-center gap-6">
+          <Typography variant="heading-4" component="p">
+            You are not connected
+          </Typography>
+          <div className="flex flex-col items-center gap-4 text-muted-foreground">
+            <Typography>
+              The Verida Vault allows you to extract your emails, messages and
+              other personal data to securely and privately store them on the
+              Verida Network and use them in AI applications.
+            </Typography>
+            <Typography>
+              Connect to your Verida Vault using your Verida Wallet.
+            </Typography>
+            <VeridaConnectButton
+              label="Connect with your Verida Wallet"
+              className="w-fit"
+            />
+          </div>
+          <div className="text-muted-foreground">
+            <Typography>
+              Learn more about{" "}
               <Link
                 href="https://www.verida.network/"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="text-primary underline"
               >
-                verida.network
+                Verida Network
               </Link>
-            </ErrorBlockDescription>
-          </ErrorBlock>
-          <VeridaConnectButton label="Connect with Verida" className="w-fit" />
+            </Typography>
+          </div>
         </div>
       </CardBody>
     </Card>
