1e589f459e8e22f37d07db95383548ae2f8399d1
aurelticot <aurelticot.dev@gmail.com>
2025-02-21 10:29:08 +1100

    feat: add onboarding on `/auth` page

--- src/features/verida-auth/components/verida-auth-verida-not-connected-card.tsx
+++ src/features/verida-auth/components/verida-auth-verida-not-connected-card.tsx
@@ -5,6 +5,7 @@ import Link from "next/link"
 import { Typography } from "@/components/typography"
 import { Card, CardBody } from "@/components/ui/card"
 import { VeridaConnectButton } from "@/components/verida/verida-connect-button"
+import { OnboardingButton } from "@/features/onboarding/components/onboarding-button"
 import { VeridaAuthCardHeader } from "@/features/verida-auth/components/verida-auth-card-header"
 import { ValidVeridaAuthRequest } from "@/features/verida-auth/types"
 import { cn } from "@/styles/utils"
@@ -25,7 +26,7 @@ export function VeridaAuthVeridaNotConnectedCard(
       <CardBody className="flex-1 overflow-y-auto pt-4 sm:pt-8">
         <div className="flex flex-col items-center gap-6">
           <Typography variant="heading-4" component="p">
-            You are not connected
+            You are not connected to your Verida Vault
           </Typography>
           <div className="flex flex-col items-center gap-4 text-muted-foreground">
             <Typography>
@@ -36,10 +37,31 @@ export function VeridaAuthVeridaNotConnectedCard(
             <Typography>
               Connect to your Verida Vault using your Verida Wallet.
             </Typography>
-            <VeridaConnectButton
-              label="Connect with your Verida Wallet"
-              className="w-fit"
-            />
+            <div className="flex flex-col gap-8">
+              <div className="flex flex-col gap-2">
+                <Typography
+                  variant="heading-5"
+                  component="p"
+                  className="text-center"
+                >
+                  New to Verida?
+                </Typography>
+                <OnboardingButton />
+              </div>
+              <div className="flex flex-col gap-2">
+                <Typography
+                  variant="heading-5"
+                  component="p"
+                  className="text-center"
+                >
+                  Already have a Verida Identity?
+                </Typography>
+                <VeridaConnectButton
+                  variant="outline"
+                  label="Connect with Verida Wallet"
+                />
+              </div>
+            </div>
           </div>
           <div className="text-muted-foreground">
             <Typography>
