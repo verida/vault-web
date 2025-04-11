72a67033746d9127c6e8b17be9dda4ecaf68adba
aurelticot <aurelticot.dev@gmail.com>
2025-04-11 10:07:01 +1000

    feat: add Verida Notification Server URL to configuration and schema

--- src/config/schemas.ts
+++ src/config/schemas.ts
@@ -34,6 +34,10 @@ export const CommonConfigSchema = z.object({
               ? Network.LOCAL
               : Network.BANKSIA
     }),
+  VERIDA_NOTIFICATION_SERVER_URL: z
+    .union([z.string().url(), z.literal("")])
+    .optional()
+    .transform((value) => (value === "" ? undefined : value)),
   VERIDA_RPC_URL: z
     .union([z.string().url(), z.literal("")])
     .optional()
